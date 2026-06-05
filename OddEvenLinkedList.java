5909000a8211b8384a65407f28e7d1192a1b3c49
Ujwala Rekha G <142649169+ujwala123rekha@users.noreply.github.com>
2026-06-05 17:25:19 +0530

    Create OddEvenLinkedList.java

---
+++ OddEvenLinkedList.java
@@ -0,0 +1,15 @@
+static Node oddEvenList(Node head) {
+    if (head == null || head.next == null) return head;
+    Node odd = head;
+    Node even = head.next;
+    Node evenHead = even; 
+    while (even != null && even.next != null) {
+        odd.next = even.next;  
+        odd = odd.next;        
+        
+        odd = odd.next;  
+        even = even.next;      
+    }
+    odd.next = evenHead;      
+    return head;
+}
